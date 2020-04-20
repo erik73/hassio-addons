3e3a416feee4f2dcdc1c521f3d479d00155db879
Erik Hilton <erikhilton73@gmail.com>
2020-04-20 21:46:12 +0200

    Fixed quota

--- mail/rootfs/usr/local/bin/quota-warning.sh
+++ mail/rootfs/usr/local/bin/quota-warning.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 PERCENT=$1
 USER=$2
-cat << EOF | /usr/libexec/dovecot/dovecot-lda -d $USER -o "plugin/quota=dict:User quota::noenforcing:proxy::sqlquota"
+cat << EOF | /usr/libexec/dovecot/dovecot-lda -d $USER -o "log_path=/proc/1/fd/2" -o "info_log_path=/proc/1/fd/1" -o "plugin/quota=dict:User quota::noenforcing:proxy::sqlquota"
 From: postmaster
 Subject: Quota warning
 
