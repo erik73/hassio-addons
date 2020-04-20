4e30fd16e33d8f11c628ea42d9bb498bfd90ae4c
Erik Hilton <erikhilton73@gmail.com>
2020-04-20 21:01:50 +0200

    Corrected spelling

--- mail/rootfs/etc/cont-init.d/11-create_config.sh
+++ mail/rootfs/etc/cont-init.d/11-create_config.sh
@@ -17,5 +17,5 @@ sed -i 's/^password .*$/password = '$SERVICE_PASSWORD'/' /etc/postfix/sql/*.cf
 sed -i 's/^hosts .*$/hosts = '$SERVICE_HOST'/' /etc/postfix/sql/*.cf
 sed -i 's/^connect .*$/connect = host='$SERVICE_HOST' dbname=postfixadmin user='$SERVICE_USERNAME' password='$SERVICE_PASSWORD'/' /etc/dovecot/*.ext
 
-sed -i "s/postmaster_address = postmaster/postmaster_address = postmaster@${domain}/g" /etc/dovecot/conf.d/20-lmpt.conf
+sed -i "s/postmaster_address = postmaster/postmaster_address = postmaster@${domain}/g" /etc/dovecot/conf.d/20-lmtp.conf
 sed -i "s/From: postmaster/From: postmaster@${domain}/g" /usr/local/bin/quota-warning.sh
