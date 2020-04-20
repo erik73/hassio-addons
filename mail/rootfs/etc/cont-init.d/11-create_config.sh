d4194459ccff2e9cfe59a1c6a7569602c85880e8
Erik Hilton <erikhilton73@gmail.com>
2020-04-20 20:52:34 +0200

    New config

--- mail/rootfs/etc/cont-init.d/11-create_config.sh
+++ mail/rootfs/etc/cont-init.d/11-create_config.sh
@@ -10,7 +10,12 @@ SERVICE_PASSWORD=$(bashio::services "mysql" "password")
 SERVICE_PORT=$(bashio::services "mysql" "port")
 SERVICE_USERNAME=$(bashio::services "mysql" "username")
 
+domain=$(bashio::config 'domain_name')
+
 sed -i 's/^user .*$/user = '$SERVICE_USERNAME'/' /etc/postfix/sql/*.cf
 sed -i 's/^password .*$/password = '$SERVICE_PASSWORD'/' /etc/postfix/sql/*.cf
 sed -i 's/^hosts .*$/hosts = '$SERVICE_HOST'/' /etc/postfix/sql/*.cf
 sed -i 's/^connect .*$/connect = host='$SERVICE_HOST' dbname=postfixadmin user='$SERVICE_USERNAME' password='$SERVICE_PASSWORD'/' /etc/dovecot/*.ext
+
+sed -i "s/postmaster_address = postmaster/postmaster_address = postmaster@${domain}/g" /etc/dovecot/conf.d/20-lmpt.conf
+sed -i "s/From: postmaster/From: postmaster@${domain}/g" /usr/local/bin/quota-warning.sh
--- mail/rootfs/usr/local/bin/quota-warning.sh
+++ mail/rootfs/usr/local/bin/quota-warning.sh
@@ -2,7 +2,7 @@
 PERCENT=$1
 USER=$2
 cat << EOF | /usr/libexec/dovecot/dovecot-lda -d $USER -o "plugin/quota=dict:User quota::noenforcing:proxy::sqlquota"
-From: postmaster@hilton.zapto.org
+From: postmaster
 Subject: Quota warning
 
 Your mailbox is now $PERCENT% full.
