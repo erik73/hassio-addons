18a54789c580e5ac9ab086409f80d02afeef4943
Erik Hilton <erikhilton73@gmail.com>
2020-04-21 10:00:35 +0200

    Revert quota changes

--- mail/rootfs/usr/local/bin/quota-warning.sh
+++ mail/rootfs/usr/local/bin/quota-warning.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 PERCENT=$1
-USER=$2
-cat << EOF | /usr/libexec/dovecot/dovecot-lda -d $USER -o "log_path=/proc/1/fd/2" -o "info_log_path=/proc/1/fd/1" -o "plugin/quota=dict:User quota::noenforcing:proxy::sqlquota"
-From: postmaster
-Subject: Quota warning
-
-Your mailbox is now $PERCENT% full.
+ USER=$2
+ cat << EOF | /usr/libexec/dovecot/dovecot-lda -d $USER -o "plugin/quota=dict:User quota::noenforcing:proxy::sqlquota"
+ From: postmaster@hilton.zapto.org
+ From: postmaster
+ Subject: Quota warning
+ 
+ Your mailbox is now $PERCENT% full.
 EOF
