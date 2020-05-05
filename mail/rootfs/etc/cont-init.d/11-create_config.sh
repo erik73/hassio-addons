04d74f1dd473a24ce595521af7ff83b85e767a3d
Erik Hilton <erikhilton73@gmail.com>
2020-05-05 05:59:54 +0200

    New version

--- mail/rootfs/etc/cont-init.d/11-create_config.sh
+++ mail/rootfs/etc/cont-init.d/11-create_config.sh
@@ -26,6 +26,10 @@ sed -i "s/myhostname =/myhostname = ${domain}/g" /etc/postfix/main.cf
 
 newaliases
 
+#Reove old sieve files so that we always have the correct ones
+
+rm -f /var/mail/vmail/sieve/global/*
+
 if ! bashio::fs.file_exists '/var/mail/vmail/sieve/global/spam-global.sieve'; then
 cat << EOF >> /var/mail/vmail/sieve/global/spam-global.sieve
 require ["fileinto","mailbox"];
@@ -43,12 +47,12 @@ EOF
 
   cat << EOF >> /var/mail/vmail/sieve/global/report-spam.sieve
 require ["vnd.dovecot.pipe", "copy", "imapsieve"];
-pipe :copy "rspamc" ["learn_spam"];
+pipe :copy "rspamc" ["-h", "32b8266a-mailfilter:11334", "learn_ham"];
 EOF
 
   cat << EOF >> /var/mail/vmail/sieve/global/report-ham.sieve
 require ["vnd.dovecot.pipe", "copy", "imapsieve"];
-pipe :copy "rspamc" ["learn_ham"];
+pipe :copy "rspamc" ["-h", "32b8266a-mailfilter:11334", "learn_ham"];
 EOF
 
 chown -R vmail:postdrop /var/mail/
@@ -57,10 +61,6 @@ fi
 
 if bashio::config.false "enable_antivirus"; then
   rm -f -r \
-  /etc/services.d/freshclam \
-  /etc/services.d/clamav \
-  /etc/services.d/rspamd \
-  /etc/services.d/redis-server \
   /etc/dovecot/conf.d/20-managesieve.conf \
   /etc/dovecot/conf.d/90-sieve.conf
 fi
@@ -71,32 +71,16 @@ if bashio::config.true "enable_antivirus"; then
 milter_protocol = 6
 milter_mail_macros = i {mail_addr} {client_addr} {client_name} {auth_authen}
 milter_default_action = accept
-smtpd_milters = inet:127.0.0.1:11332
-non_smtpd_milters = inet:127.0.0.1:11332
+smtpd_milters = inet:32b8266a-mailfilter:11332
+non_smtpd_milters = inet:32b8266a-mailfilter:11332
 EOF
 
     sed -i 's/^  mail.*/& sieve/' /etc/dovecot/conf.d/20-lmtp.conf
     sed -i 's/^  mail.*/& imap_sieve/' /etc/dovecot/conf.d/20-imap.conf
-    mkdir -p /data/lib/rspamd
-    mkdir -p /data/lib/redis
-    mkdir -p /data/lib/clamav
-    rm -fr /var/lib/rspamd
-    rm -fr /var/lib/redis
-    rm -fr /var/lib/clamav
-    ln -s /data/lib/rspamd /var/lib/rspamd
-    ln -s /data/lib/redis /var/lib/redis
-    chown rspamd:rspamd /var/lib/rspamd
-    chown redis:redis /var/lib/redis
     # Disable DKIM Signing
     # mkdir -p /var/lib/rspamd/dkim/
     # chown rspamd:rspamd /var/lib/rspamd/dkim
     # rspamadm dkim_keygen -b 2048 -s mail -k /var/lib/rspamd/dkim/mail.key | tee -a  /var/lib/rspamd/dkim/mail.pub
     # chown -R rspamd:rspamd /var/lib/rspamd/dkim
-    mkdir /run/clamav
-    chown clamav:clamav /run/clamav
-    ln -s /data/lib/clamav /var/lib/clamav
-    chown clamav:clamav /var/lib/clamav
-    mkdir /run/rspamd
-    bashio::log.info "Updating antivirus patterns"
-    freshclam >/dev/null 2>&1
+
 fi
