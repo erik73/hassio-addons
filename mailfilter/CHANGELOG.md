cef6e2680be01bfbfc3f96155450db286978661f
Erik Hilton <erikhilton73@gmail.com>
2020-05-16 16:30:58 +0200

    New clamav version and config changes

--- mailfilter/CHANGELOG.md
+++ mailfilter/CHANGELOG.md
@@ -1,5 +1,13 @@
 # Changelog
 
+## 0.1.7
+
+ - Clean up config files and use clamav update to 0.102.3
+
+## 0.1.6
+
+ - Build Rspamd with jemalloc
+
 ## 0.1.5
 
  - Created changelog
--- mailfilter/rootfs/etc/cont-init.d/10-create_config.sh
+++ mailfilter/rootfs/etc/cont-init.d/10-create_config.sh
@@ -12,28 +12,33 @@
     ln -s /data/lib/redis /var/lib/redis
     chown rspamd:rspamd /var/lib/rspamd
     chown redis:redis /var/lib/redis
+    mkdir /run/clamav
+    chown clamav:clamav /run/clamav
+    ln -s /data/lib/clamav /var/lib/clamav
+    chown clamav:clamav /var/lib/clamav
+    mkdir /run/rspamd
+    
     # Disable DKIM Signing
     # mkdir -p /var/lib/rspamd/dkim/
     # chown rspamd:rspamd /var/lib/rspamd/dkim
     # rspamadm dkim_keygen -b 2048 -s mail -k /var/lib/rspamd/dkim/mail.key | tee -a  /var/lib/rspamd/dkim/mail.pub
     # chown -R rspamd:rspamd /var/lib/rspamd/dkim
 
-#Create rspamd encrypted password
+#Create rspamd encrypted password and set listening IP
 rspamdpw="$(date | md5sum)"
 encryptedpw="$(rspamadm pw --encrypt -p ${rspamdpw})"
 encryptedenpw="$(rspamadm pw --encrypt -p ${rspamdpw})"
+myip="$(ip route get 1 | awk '{print $NF;exit}')"
 sed -i "4 s/password = /password = "${encryptedpw}";/g" /etc/rspamd/local.d/worker-controller.inc
 sed -i "5 s/enable_password = /enable_password = "${encryptedenpw}";/g" /etc/rspamd/local.d/worker-controller.inc
+sed -i '2 s/^bind.*$/bind_socket = "'${myip}:11332'";/g' /etc/rspamd/local.d/worker-proxy.inc
 
+#Remove antivirus service files if disabled
 if bashio::config.false "enable_antivirus"; then
     rm -fr /etc/services.d/clamav
     rm -fr /etc/services.d/freshclam
 fi
-    mkdir /run/clamav
-    chown clamav:clamav /run/clamav
-    ln -s /data/lib/clamav /var/lib/clamav
-    chown clamav:clamav /var/lib/clamav
-    mkdir /run/rspamd
+
 if bashio::config.true "enable_antivirus"; then
     sed -i '1d' /etc/rspamd/local.d/antivirus.conf
     bashio::log.info "Updating antivirus patterns"
