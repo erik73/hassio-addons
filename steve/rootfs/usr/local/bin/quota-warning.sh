2d32130ea7311fd625740c6f8d6038ca5ace83f4
Erik Hilton <erikhilton73@gmail.com>
2021-01-03 18:30:26 +0100

    Initial commit

--- steve/rootfs/usr/local/bin/quota-warning.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-PERCENT=$1
-USER=$2
-cat << EOF | /usr/libexec/dovecot/dovecot-lda -d $USER -o "plugin/quota=dict:User quota::noenforcing:proxy::sqlquota"
-From: postmaster
-Subject: Quota warning
-
-Your mailbox is now $PERCENT% full.
-EOF
