41c9678f474d81c79973dc5fbb4d085d9c3fed11
Erik Hilton <erikhilton73@gmail.com>
2021-01-03 22:36:37 +0100

    Fix text replacements

--- steve/rootfs/etc/cont-init.d/10-create-config.sh
+++ steve/rootfs/etc/cont-init.d/10-create-config.sh
@@ -6,6 +6,6 @@
 
 
 # Modify config files
-sed -i 's/^db.ip .*$/db.ip = core-mariadb/' usr/src/steve/src/main/resources/config/prod/main.properties
-sed -i 's/^db.user .*$/db.user = stevedbuser/' usr/src/steve/src/main/resources/config/prod/main.properties
-sed -i 's/^db.password .*$/db.password = Nisse123/' usr/src/steve/src/main/resources/config/prod/main.properties
+sed -i 's/^db.ip .*$/db.ip = core-mariadb/' /usr/src/steve/src/main/resources/config/prod/main.properties
+sed -i 's/^db.user .*$/db.user = stevedbuser/' /usr/src/steve/src/main/resources/config/prod/main.properties
+sed -i 's/^db.password .*$/db.password = Nisse123/' /usr/src/steve/src/main/resources/config/prod/main.properties
