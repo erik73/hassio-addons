89d1a2d30e460828b7a1d20e7bb03bbcf4c3cd09
Erik Hilton <erikhilton73@gmail.com>
2021-01-03 23:34:16 +0100

    Fix config file path

--- steve/rootfs/etc/cont-init.d/10-create-config.sh
+++ steve/rootfs/etc/cont-init.d/10-create-config.sh
@@ -9,4 +9,4 @@
 sed -i 's/^db.ip .*$/db.ip = core-mariadb/' /usr/src/steve/src/main/resources/config/prod/main.properties
 sed -i 's/^db.user .*$/db.user = stevedbuser/' /usr/src/steve/src/main/resources/config/prod/main.properties
 sed -i 's/^db.password .*$/db.password = Nisse123/' /usr/src/steve/src/main/resources/config/prod/main.properties
-sed -i 's/^private final String timeZoneId .*$/private final String timeZoneId = ZoneId.systemDefault().getId();/' /usr/src/main/java/de/rwth/idsg/steve/SteveConfiguration.java
+sed -i 's/^private final String timeZoneId .*$/private final String timeZoneId = ZoneId.systemDefault().getId();/' /usr/src/steve/src/main/java/de/rwth/idsg/steve/SteveConfiguration.java
