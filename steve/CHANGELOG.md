747bd3e11e682d9b4c6f0993bd3d3b3aac3f79c3
Erik Hilton <erikhilton73@gmail.com>
2021-01-04 17:11:45 +0100

    bump version

--- steve/CHANGELOG.md
+++ steve/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.1.2
+
+ - New version with better config options handling
+
 ## 0.1.1
 
  - Corrected config options
--- steve/rootfs/etc/cont-init.d/10-create-config.sh
+++ steve/rootfs/etc/cont-init.d/10-create-config.sh
@@ -4,12 +4,41 @@
 # Configures SteVe
 # ==============================================================================
 
+export host
+export password
+export port
+export username
+export database
+
+host=$(bashio::services "mysql" "host")
+password=$(bashio::services "mysql" "password")
+port=$(bashio::services "mysql" "port")
+username=$(bashio::services "mysql" "username")
+adminuser=$(bashio::config 'admin_user')
+adminpassword=$(bashio::config 'admin_password')
+timezone=$(bashio::config 'timezone')
 
 # Modify config files
-sed -i 's/^db.ip .*$/db.ip = core-mariadb/' /usr/src/steve/src/main/resources/config/prod/main.properties
-sed -i 's/^db.user .*$/db.user = stevedbuser/' /usr/src/steve/src/main/resources/config/prod/main.properties
-sed -i 's/^db.password .*$/db.password = Nisse123/' /usr/src/steve/src/main/resources/config/prod/main.properties
+sed -i 's/^db.ip .*$/db.ip = '$host'/' /usr/src/steve/src/main/resources/config/prod/main.properties
+sed -i 's/^db.user .*$/db.user = '$username'/' /usr/src/steve/src/main/resources/config/prod/main.properties
+sed -i 's/^db.password .*$/db.password = '$password'/' /usr/src/steve/src/main/resources/config/prod/main.properties
 sed -i 's/^server.host .*$/server.host = 0.0.0.0/' /usr/src/steve/src/main/resources/config/prod/main.properties
 sed -i 's/^http.port .*$/http.port = 8180/' /usr/src/steve/src/main/resources/config/prod/main.properties
-# sed -i 's/^    private final String timeZoneId .*$/    private final String timeZoneId = ZoneId.systemDefault().getId();/' /usr/src/steve/src/main/java/de/rwth/idsg/steve/SteveConfiguration.java
-sed -i 's/^    private final String timeZoneId .*$/    private final String timeZoneId = "CET";/' /usr/src/steve/src/main/java/de/rwth/idsg/steve/SteveConfiguration.java
+sed -i 's/^auth.user .*$/auth.user = '$adminuser'/' /usr/src/steve/src/main/resources/config/prod/main.properties
+sed -i 's/^auth.password .*$/auth.password = '$adminpassword'/' /usr/src/steve/src/main/resources/config/prod/main.properties
+sed -i 's/^    private final String timeZoneId .*$/    private final String timeZoneId = "'$timezone'";/' /usr/src/steve/src/main/java/de/rwth/idsg/steve/SteveConfiguration.java
+
+database=$(\
+    mysql \
+        -u "${username}" -p"${password}" \
+        -h "${host}" -P "${port}" \
+        --skip-column-names \
+        -e "SHOW DATABASES LIKE 'stevedb';"
+)
+
+if ! bashio::var.has_value "${database}"; then
+    bashio::log.info "Creating database for SteVe"
+    mysql \
+        -u "${username}" -p"${password}" \
+        -h "${host}" -P "${port}" \
+            < /usr/src/steve/createdb.sql
