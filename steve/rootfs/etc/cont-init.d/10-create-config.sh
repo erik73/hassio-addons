400f003f3404f7c9906aeceb254c1163b26f48b2
Erik Hilton <erikhilton73@gmail.com>
2021-01-04 08:37:59 +0100

    Change server IP options

--- steve/rootfs/etc/cont-init.d/10-create-config.sh
+++ steve/rootfs/etc/cont-init.d/10-create-config.sh
@@ -9,5 +9,7 @@
 sed -i 's/^db.ip .*$/db.ip = core-mariadb/' /usr/src/steve/src/main/resources/config/prod/main.properties
 sed -i 's/^db.user .*$/db.user = stevedbuser/' /usr/src/steve/src/main/resources/config/prod/main.properties
 sed -i 's/^db.password .*$/db.password = Nisse123/' /usr/src/steve/src/main/resources/config/prod/main.properties
+sed -i 's/^server.host .*$/server.host = 0.0.0.0/' /usr/src/steve/src/main/resources/config/prod/main.properties
+sed -i 's/^http.port .*$/http.port = 8180/' /usr/src/steve/src/main/resources/config/prod/main.properties
 # sed -i 's/^    private final String timeZoneId .*$/    private final String timeZoneId = ZoneId.systemDefault().getId();/' /usr/src/steve/src/main/java/de/rwth/idsg/steve/SteveConfiguration.java
 sed -i 's/^    private final String timeZoneId .*$/    private final String timeZoneId = "CET";/' /usr/src/steve/src/main/java/de/rwth/idsg/steve/SteveConfiguration.java
