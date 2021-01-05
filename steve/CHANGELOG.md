aa6d9c1f38e26394f0ad750d344c9a8eee33451a
Erik Hilton <erikhilton73@gmail.com>
2021-01-05 12:35:53 +0100

    New version

--- steve/CHANGELOG.md
+++ steve/CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## 0.2.0
+
+ - Stop SteVe from building unless there is a new addon version, or build har not run yet
+ - Add config option to set http port
+
 ## 0.1.2
 
  - New version with better config options handling
--- steve/rootfs/etc/cont-init.d/10-create-config.sh
+++ steve/rootfs/etc/cont-init.d/10-create-config.sh
@@ -17,13 +17,14 @@ username=$(bashio::services "mysql" "username")
 adminuser=$(bashio::config 'admin_user')
 adminpassword=$(bashio::config 'admin_password')
 timezone=$(bashio::config 'timezone')
+httpport=$(bashio::config 'httpport')
 
 # Modify config files
 sed -i 's/^db.ip .*$/db.ip = '$host'/' /usr/src/steve/src/main/resources/config/prod/main.properties
 sed -i 's/^db.user .*$/db.user = '$username'/' /usr/src/steve/src/main/resources/config/prod/main.properties
 sed -i 's/^db.password .*$/db.password = '$password'/' /usr/src/steve/src/main/resources/config/prod/main.properties
 sed -i 's/^server.host .*$/server.host = 0.0.0.0/' /usr/src/steve/src/main/resources/config/prod/main.properties
-sed -i 's/^http.port .*$/http.port = 8180/' /usr/src/steve/src/main/resources/config/prod/main.properties
+sed -i 's/^http.port .*$/http.port = '$httpport'/' /usr/src/steve/src/main/resources/config/prod/main.properties
 sed -i 's/^auth.user .*$/auth.user = '$adminuser'/' /usr/src/steve/src/main/resources/config/prod/main.properties
 sed -i 's/^auth.password .*$/auth.password = '$adminpassword'/' /usr/src/steve/src/main/resources/config/prod/main.properties
 sed -i 's/^    private final String timeZoneId .*$/    private final String timeZoneId = "'$timezone'";/' /usr/src/steve/src/main/java/de/rwth/idsg/steve/SteveConfiguration.java
