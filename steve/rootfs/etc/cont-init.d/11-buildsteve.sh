c91ea531b370be9c4a53257f2cf868972e64954d
Erik Hilton <erikhilton73@gmail.com>
2021-01-05 13:39:51 +0100

    Fix build

--- steve/rootfs/etc/cont-init.d/11-buildsteve.sh
+++ steve/rootfs/etc/cont-init.d/11-buildsteve.sh
@@ -9,7 +9,7 @@ export MAVEN_OPTS="-Xmx100m"
 if ! bashio::fs.file_exists '/data/steve.jar'; then
     bashio::log.info "Starting SteVe initial build...."
     cd /usr/src/steve
-    exec mvn package
+    mvn package
     cp -f /usr/src/steve/pom.xml /data/
     cp -f /usr/src/steve/target/steve.jar /data/
 fi
@@ -18,7 +18,7 @@ diff /usr/src/steve/pom.xml /data/pom.xml
 if [ $? -ne 0 ]; then
     bashio::log.info "Starting SteVe rebuild since there is a new verion...."
     cd /usr/src/steve
-    exec mvn package
+    mvn package
     cp -f /usr/src/steve/pom.xml /data/
     cp -f /usr/src/steve/target/steve.jar /data/
 fi
