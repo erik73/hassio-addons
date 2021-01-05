eddcf2e34bb2ce8e35d58ba8ed0bbb4c0ac5d202
Erik Hilton <erikhilton73@gmail.com>
2021-01-05 17:18:29 +0100

    Improve memory usage

--- steve/rootfs/etc/cont-init.d/11-buildsteve.sh
+++ steve/rootfs/etc/cont-init.d/11-buildsteve.sh
@@ -4,12 +4,10 @@
 # Check if SteVe needs to be built
 # ==============================================================================
 
-export MAVEN_OPTS="-Xmx100m"
-
 if ! bashio::fs.directory_exists '/data/target'; then
     bashio::log.info "Starting SteVe initial build...."
     cd /usr/src/steve
-    mvn package
+    MAVEN_OPTS="-Xmx100m" mvn package
     cp -f /usr/src/steve/pom.xml /data/
     cp -R -f /usr/src/steve/target/ /data/
 fi
@@ -18,7 +16,7 @@ diff /usr/src/steve/pom.xml /data/pom.xml
 if [ $? -ne 0 ]; then
     bashio::log.info "Starting SteVe rebuild since there is a new verion...."
     cd /usr/src/steve
-    mvn package
+    MAVEN_OPTS="-Xmx100m" mvn package
     rm -rf /data/target
     cp -f /usr/src/steve/pom.xml /data/
     cp -R -f /usr/src/steve/target/ /data/
