b96ae0a0c3d96c4cdc94039ea861017a3ddd8117
Erik Hilton <erikhilton73@gmail.com>
2021-01-07 18:44:19 +0100

    Create new config file

--- steve/rootfs/etc/cont-init.d/12-rebuildsteve.sh
+++ steve/rootfs/etc/cont-init.d/12-rebuildsteve.sh
@@ -4,11 +4,14 @@
 # Check if SteVe needs to be built
 # ==============================================================================
 
-if ! bashio::fs.directory_exists '/data/target'; then
-    bashio::log.info "Starting SteVe initial build...."
+builtversion="$(xmlstarlet sel -N x=http://maven.apache.org/POM/4.0.0 -t -v "/x:project/x:version" /data/pom.xml)"
+packageversion="$(xmlstarlet sel -N x=http://maven.apache.org/POM/4.0.0 -t -v "/x:project/x:version" /usr/src/steve/pom.xml)"
+
+if [ "$builtversion" != "$packageversion" ]; then
+    bashio::log.info "Starting SteVe rebuild since there is a new verion...."
     cd /usr/src/steve
     MAVEN_OPTS="-Xmx100m" mvn package
+    rm -rf /data/target
     cp -f /usr/src/steve/pom.xml /data/
     cp -R -f /usr/src/steve/target/ /data/
 fi
-
